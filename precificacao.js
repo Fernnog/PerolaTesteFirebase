ddb0e2f1f0561673eef76936fb5e738a61ed2506
Fernnog <fernandocnogueira@gmail.com>
2025-02-15 23:27:43 -0400

    Update precificacao.js

--- precificacao.js
+++ precificacao.js
@@ -4,16 +4,16 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { getFirestore, collection, doc, setDoc, getDocs, updateDoc, deleteDoc, query, where, orderBy, getDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
-// Suas configurações do Firebase (ATUALIZADAS PARA O SEU PROJETO)
+// Suas configurações do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://SEU_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID",
-  measurementId: "SEU_MEASUREMENT_ID"
+  apiKey: "AIzaSyAydkMsxydduoAFD9pdtg_KIFuckA_PIkE",
+  authDomain: "precificacao-64b06.firebaseapp.com",
+  databaseURL: "https://precificacao-64b06-default-rtdb.firebaseio.com",
+  projectId: "precificacao-64b06",
+  storageBucket: "precificacao-64b06.firebasestorage.app",
+  messagingSenderId: "872035099760",
+  appId: "1:872035099760:web:1c1c7d2ef0f442b366c0b5",
+  measurementId: "G-6THHCNMHD6"
 };
 
 // Inicializar o Firebase
